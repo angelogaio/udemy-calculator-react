0b695c521d04d78d4eaba662f918b2d9c5702b89
Angelo <angelo.gaio@hotmail.com>
2024-03-25 19:29:14 -0300

    implementacao

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
   <React.StrictMode>
+    <h1>Calculadora</h1>
     <Calculator />
   </React.StrictMode>
+  </div>
 );
 reportWebVitals();
